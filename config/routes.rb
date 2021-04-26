bf7257f50fdeec747e30d72a05a8a5b09da0d2ac
Naren404 <narendrasunar47@gmail.com>
2021-04-26 10:04:24 +0545

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+ 
+  scope '/api/v1' do
+    resources :todos
+  end
+  
+end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Todo.create(title: "Buy food: milk, bread, fruits", done: false)
+Todo.create(title: "Imagine Dragons tickets", done: false)
